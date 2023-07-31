4160cdf46d020ec454ec4d19f08415224c8a533a
john-kick <andre010400@web.de>
2023-08-01 00:42:31 +0200

    Adds docs to Pixel and Strip classes

--- src/backend/Pixel.ts
+++ src/backend/Pixel.ts
@@ -5,12 +5,21 @@ export default class Pixel {
     g!: number;
     b!: number;
 
+    /**
+     * @param r Color value for red
+     * @param g Color value for green
+     * @param b Color value for purple
+     */
     constructor(r: number = 0, g: number = 0, b: number = 0) {
         this.setR(r);
         this.setG(g);
         this.setB(b);
     }
 
+    /**
+     * @param color The RGB object to set the pixels color to
+     * @returns The Pixel object with the changed color
+     */
     public setColor(color: RGB): this {
         this.setR(color.r);
         this.setG(color.g);
@@ -18,24 +27,39 @@ export default class Pixel {
         return this;
     }
 
+    /**
+     * @param r Color value for red. Must be between 0-255
+     * @returns The Pixel object with the changed color
+     */
     public setR(r: number): this {
         if (r < 0 || r > 255) throw new Error("r must be between 0 and 255");
         this.r = r;
         return this;
     }
 
+    /**
+     * @param g Color value for green. Must be between 0-255
+     * @returns The Pixel object with the changed color
+     */
     public setG(g: number): this {
         if (g < 0 || g > 255) throw new Error("g must be between 0 and 255");
         this.g = g;
         return this;
     }
 
+    /**
+     * @param b Color value for blue. Must be between 0-255
+     * @returns The Pixel object with the changed color
+     */
     public setB(b: number): this {
         if (b < 0 || b > 255) throw new Error("b must be between 0 and 255");
         this.b = b;
         return this;
     }
 
+    /**
+     * @param x The brightness value of the pixel. Must be between 0-255
+     */
     public setBrightness(x: number) {
         if (x < 0 || x > 255) throw new Error("Brightness needs to be between 0 and 255");
         const rel = x / 255;
--- src/backend/Strip.ts
+++ src/backend/Strip.ts
@@ -1,6 +1,9 @@
 import Pixel from "./Pixel";
 import { RGB } from "./util";
 
+/**
+ * Represents an LED Strip with 'length' amount of LEDs, stored in 'pixels'
+ */
 export default class Strip {
     pixels: Pixel[];
     length: number;
@@ -15,6 +18,9 @@ export default class Strip {
         }
     }
 
+    /**
+     * @returns Bit Array containing the colors r, g and b values (0-255), so 3 * 'length' values
+     */
     public toUint8Array(): Uint8Array {
         // Apply brightness
         const bPixels = this.adaptBrightness();
@@ -31,33 +37,67 @@ export default class Strip {
         return arr;
     }
 
+    /**
+     * @param i Pixel at this position
+     * @returns Instance of the pixel at this position in pixel array
+     */
     public getPixel(i: number): Pixel {
         return this.pixels[i];
     }
 
+    /**
+     * @param i Replaces Pixel object at this position in pixels array
+     * @param pixel The replacement pixel
+     */
     public setPixel(i: number, pixel: Pixel) {
         this.pixels[i] = pixel;
     }
 
+    /**
+     * @param i Pixel at this position in the pixels array
+     * @param color The RGB object containing r, g and b values of the new color
+     */
     public setPixelColor(i: number, color: RGB) {
         this.pixels[i].setColor(color);
     }
 
+    /**
+     * @param i Pixel at this position in the pixels array
+     * @param x The brightness of this pixel. Must be between 0-255
+     */
+    public setPixelBrightness(i: number, x: number) {
+        if (x < 0 || x > 255) throw new Error("Brightness needs to be between 0 and 255");
+        this.pixels[i].setBrightness(x);
+    }
+
+    /**
+     * Sets the color of every pixel in the pixels array with this color
+     * @param color The color to fill the strip with
+     */
     public fill(color: RGB) {
         this.pixels.forEach((pixel) => {
             pixel.setColor(color);
         });
     }
 
+    /**
+     * Sets the color of each pixel in the pixels array to r=0, g=0, b=0
+     */
     public clear() {
         this.fill({ r: 0, g: 0, b: 0 });
     }
 
+    /**
+     * @param x The brightness to set the pixels to. Must be 0-255
+     */
     public setBrightness(x: number) {
         if (x < 0 || x > 255) throw new Error("Brightness needs to be between 0 and 255");
         this.brightness = x;
     }
 
+    /**
+     * @returns The pixel array, but adapting each pixel to the stored brightness
+     */
     private adaptBrightness(): Pixel[] {
         const rel = this.brightness / 255;
         const bPixels: Pixel[] = [];
